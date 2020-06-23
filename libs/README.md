5f26c805e66e6ba2d606b42c4265aaabc8a57676
2554509132@qq.com <2554509132@qq.com>
2020-06-23 20:00:57 +0800

    修改函数头注释,修改函数变量名

--- libs/README.md
+++ libs/README.md
@@ -1,10 +1,26 @@
 <!--
  * @Author: your name
  * @Date: 2020-06-22 20:41:43
- * @LastEditTime: 2020-06-22 20:42:01
+ * @LastEditTime: 2020-06-23 19:57:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /learnNodeAgain/libs/README.md
 --> 
 # 存放API相关代码
+* 新增文件拷贝功能
+```
+/**
+ * @description: 大文件拷贝 
+ * @param {URL} 源路径
+ * @param {URL} 目标路径
+ */
+my_copy(src, dest){ ... }
+
+/**
+ * @description: 小文件拷贝 
+ * @param {URL} 源路径
+ * @param {URL} 目标路径
+ */
+my_copy(src, dest){ ... }
+```
 
--- libs/myFileSys.js
+++ libs/myFileSys.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-23 15:21:19
- * @LastEditTime: 2020-06-23 16:22:13
+ * @LastEditTime: 2020-06-23 19:57:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /learnNodeAgain/libs/myFileSys.js
@@ -9,11 +9,11 @@
 const fs = require('fs');
 
 /**
- * @description: 小文件拷贝
- * @param {URL} 目标路径
+ * @description: 小文件拷贝 
  * @param {URL} 源路径
+ * @param {URL} 目标路径
  */
-let my_copy_sync = (src, destPath) => {
+let my_copy_sync = (src, dest) => {
     //一次性将文件读入到内存中，再一次性写入
     // 问题是：文件过大的话，内存会爆仓
     fs.writeFileSync(destPath, fs.readFileSync(src));
@@ -21,10 +21,10 @@ let my_copy_sync = (src, destPath) => {
 
 /**
  * @description: 大文件拷贝
- * @param {URL} 目标路径
  * @param {URL} 源路径
+ * @param {URL} 目标路径
  */
-let my_copy = (src, destPath) => {
+let my_copy = (src, dest) => {
     // 蚂蚁搬家式拷贝，读一点，写一点
     fs.createReadStream(src).pipe(fs.createWriteStream(destPath));
 };
