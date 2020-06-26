3ff65ebf76e6c09d554b6f7f471fc94c87070b9d
2554509132@qq.com <2554509132@qq.com>
2020-06-26 11:37:08 +0800

    调用异步目录遍历

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-23 15:17:02
- * @LastEditTime: 2020-06-23 16:58:04
+ * @LastEditTime: 2020-06-26 11:04:44
  * @LastEditors: Please set LastEditors
  * @Description: 项目主模块，
  * @FilePath: /learnNodeAgain/main.js
@@ -11,6 +11,8 @@ const {
     myFileSys: {
         my_copy_sync: my_copy_sync,
         my_copy: my_copy,
+        travel: travel,
+        travel_sync: travel_sync
     }
 } = require('./libs');
 
@@ -18,22 +20,20 @@ const {
 var param = process.argv.slice(2);
 // 执行复制函数
 // my_copy_sync(param[0], param[1]); // 小文件拷贝
-my_copy(param[0], param[1]); //大文件拷贝
+// my_copy(param[0], param[1]); //大文件拷贝
 
-/* 
-process 是一个全局变量，类似 __dirname 和 __dirpath 
-process.argv 获取命令行参数，
-process.argv[0] 固定等于 NodeJS 执行程序的绝对路径，就是 node 所在的路径，
-process.argv[1] 固定等于主模块的绝对路径，就是 main.js 的位置
-因此第一个命令行参数从 process.argv[2]这个位置开始
-*/
+// travel_sync(param[0], result => {
+//     console.log(result);
+//     // 文件处理
+// });
+
+travel(param[0], (file_path, callback) => {
+    console.log(file_path);
+    callback();
+});
 
 /* 
 fs.createReadStream 创建一个源文件的只读数据流
 fs.createWriteStream 创建一个目标文件的只写数据流
 使用 pipe 方法把两个数据流连接起来
-*/
-
-/* 
-
 */
